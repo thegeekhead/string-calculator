2d41a7daf0c71c97d85dd2f0930ef48a0903ceda
Anshansh Suman <anshanshsuman@Anshanshs-MacBook-Air.local>
2025-09-18 18:45:26 +0530

    refactor: clean up implementation for handling unknown numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,10 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    const nums = numbers.split(",").map(Number);
-
-    return nums.reduce((sum, num) => sum + num, 0);
+    return numbers
+        .split(",")
+        .map(num => parseInt(num, 10))
+        .reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
