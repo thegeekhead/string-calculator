5dd20e1147d39e68ec9bf2383ddf1890d0f03099
Anshansh Suman <anshanshsuman@Anshanshs-MacBook-Air.local>
2025-09-18 18:23:28 +0530

    test: Add returns sum of two comma-separated numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,3 +7,7 @@ test('returns 0 for empty string', () => {
 test('returns number itself when single number provided', () => {
     expect(add('5')).toBe(5);
 });
+
+test('returns sum of two comma-separated numbers', () => {
+    expect(add('1,2')).toBe(3);
+});
