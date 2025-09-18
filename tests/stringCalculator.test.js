bb6a7361b88d1145229a9c3f53568c94623c6272
Anshansh Suman <anshanshsuman@Anshanshs-MacBook-Air.local>
2025-09-18 18:16:20 +0530

    test: Add returns the number itself for single input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require('../src/stringCalculator');
 test('returns 0 for empty string', () => {
     expect(add('')).toBe(0);
 });
+
+test('returns number itself when single number provided', () => {
+    expect(add('5')).toBe(5);
+});
