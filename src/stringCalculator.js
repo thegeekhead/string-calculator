ecae81a56077ca6b892f445b220695fde0e6293b
Anshansh Suman <anshanshsuman@Anshanshs-MacBook-Air.local>
2025-09-18 18:43:28 +0530

    feat: support summing an unknown amount of numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,12 +1,9 @@
 function add(numbers) {
-    if (numbers === '') return 0;
+    if (numbers === "") return 0;
 
-    const parts = numbers.split(',').map(Number);
+    const nums = numbers.split(",").map(Number);
 
-    if (parts.length === 1) return parts[0];
-    if (parts.length === 2) return parts[0] + parts[1];
-
-    return 0; // will evolve for multiple numbers later
+    return nums.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
