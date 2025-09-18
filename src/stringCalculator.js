ae11dab10cf1b8f0cf4aa3b42f1b074c33fd1d5e
Anshansh Suman <anshanshsuman@Anshanshs-MacBook-Air.local>
2025-09-18 18:48:05 +0530

    feat: support newlines as delimiters along with commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,7 @@ function add(numbers) {
     if (numbers === "") return 0;
 
     return numbers
-        .split(",")
+        .split(/[\n,]/) // split by comma OR newline
         .map(num => parseInt(num, 10))
         .reduce((sum, num) => sum + num, 0);
 }
